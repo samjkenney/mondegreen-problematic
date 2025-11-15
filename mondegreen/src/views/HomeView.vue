5e1ad8d411792347e0957e3e2127780ff534ab6f
samjkenney <129895815+samjkenney@users.noreply.github.com>
2025-11-15 11:12:13 -0600

    reverting back to genius

--- mondegreen/src/views/HomeView.vue
+++ mondegreen/src/views/HomeView.vue
@@ -1,15 +1,44 @@
 <template>
   <main>
-    <h2 id = "search"> Available Songs: </h2>
+    <router-link to="/login" aria-label="Log In" id="userLink">
+      <button>Log In</button>
+    </router-link>
+    <h2 id="search">Search for a song:</h2>
     <p>{{ message2 }}</p>
-    <div class = "songcontainerHome">
-      <div class = "songHome" v-for="item in scores" :key="item">
-      <ul>
-        <p> {{ getTitle(item) }}</p>
-        <p> {{ getArtist(item) }}</p>
-        <button @click="playSong(item)">Play Song</button>
-        <p>{{ message5 }}</p>
-      </ul>
+    <div >
+      <input v-on:keyup.enter="generalSearch" type="text" v-model="searchGeneral" placeholder="Enter Song" />
+      <button @click="generalSearch">Search</button>
+      <div class="songcontainerHome">
+        <div class = "songHome" v-for="item in scores" :key="item.title">
+            <img :src="item.song_art_image_thumbnail_url" :alt="item.title">
+            <ul>
+              <p>{{ item.title }}</p>
+              <p>{{ item.artist }}</p>
+              <button @click="playSong(item,item.title,item.artist,false)">Play Song</button>
+              <p>{{ message5 }}</p>
+            </ul>
+            
+        </div>
+    </div>
+    </div>
+    <div id="recs" v-if="showRecs">
+      <h3>Want to be recommended some songs? Select a genre:</h3> 
+      <p>{{ message3 }}</p>
+      <div class = "genreButtons">
+      <div v-for="[key,value] in Object.entries(genres)" :key="key">
+        <button class="genre" @click="recommended(key, value)">{{ value }}</button>
+      </div>
+    </div>
+  </div>
+
+    <div class="songcontainerHome">
+      <div class = "songHome" v-for="item in recommendation" :key="item.title">
+          <ul>
+            <p>{{ item.title }}</p>
+            <p>{{ item.artist }}</p>
+            <button @click="playSong(item,item.title,item.artist,true)">Play Song</button>
+            <p>{{ message5 }}</p>
+          </ul>
       </div>
     </div>
     <p>Just starting?</p>
@@ -25,71 +54,149 @@ import axios from 'axios';
 import store from '../store';
 import HowToView from './HowToView.vue';
 
-
 export default {
   components: { HowToView },
   name: "HomeView",
   data() {
     return {
-      message2: '',
-      message5: '',
+      searchTitle: "",
+      searchArtist: "",
+      searchGeneral: "",
+      lyrics: "",
+      message: "",
+      message2: "",
+      // list of songs returned from the search
       scores: [],
-      howTo: false
+      genres: {
+        "rap": "Rap",
+        "pop": "Pop",
+        "r-b": "R & B",
+        "rock": "Rock",
+        "country": "Country",
+        "non-music": "Non-Music"
+      },
+      message3: "",
+      // list of recommended songs based on the genre
+      recommendation: [], 
+      message5: "",
+      showRecs: true,
+      howTo: false,
     };
-  },
-  created() {
-    console.log('Loading songs');
-      axios.get(`https://those-are-the-lyrics-fe11728950ff.herokuapp.com/lyrics/list`)
-        .then(response => {
-          this.scores = response.data.list;
-      })
-      .catch(error => {
-      this.message2 = 'Failed to fetch songs';
-      })
   },
   methods: {
     toggleHowTo() {
-      console.log('Toggle!');
       this.howTo = !this.howTo;
     },
-    getTitle(item){
-      const strSplit = item.split(':');
-      const title = strSplit[0];
-      const clean_title = title.replaceAll("-", " ");
-      return clean_title;
-    },
-    getArtist(item){
-      const strSplit = item.split(':');
-      const artist = strSplit[1];
-      const clean_artist = artist.replaceAll("-", " ");
-      return clean_artist;
-    },
-    playSong(item) {
-      this.message2 = '';
-      this.message5 = `Starting game: "${item}"`;
-      const strSplit = item.split(':');
-      const raw_title = strSplit[0];
-      const title = raw_title.replaceAll("-", " ");
-      const raw_artist = strSplit[1];
-      const artist = raw_artist.replaceAll("-", " ");
-      const url = `https://those-are-the-lyrics-fe11728950ff.herokuapp.com/lyrics/${encodeURIComponent(raw_title.toLowerCase())}/${encodeURIComponent(raw_artist.toLowerCase())}` 
-      console.log(url)
-      axios.get(url)
-        .then(response => {
-          
-          const lyrics = response.data.lyrics;
-          store.commit('setLyrics', lyrics);
-          store.commit('setTitle', title);
-          store.commit('setArtist', artist);
-          this.$router.push({
-            path: '/game'
+    // function to search for a song by title and artist; more specific and requires accurate input 
+    // no longer used
+    submitSearch() {
+      const title = this.searchTitle.trim();
+      const artist = this.searchArtist.trim();
+      this.message = `Searching for "${title}" by "${artist}"...`;
+      
+
+      if (title && artist) {
+        axios.get(`http://localhost:5001/lyrics/${encodeURIComponent(title)}/${encodeURIComponent(artist)}`)
+          .then(response => {
+            this.message = '';
+            if (response.data.lyrics === "Lyrics not found") {
+              this.message = "Lyrics not found";
+            } else {
+              this.message = `Found lyrics for "${title}" by "${artist}"`;
+              store.commit('setLyrics', response.data.lyrics);
+              store.commit('setTitle', title);
+              store.commit('setArtist', artist);
+              this.$router.push({
+                path: '/game'
+              });
+            }
+          })
+          .catch(error => {
+            this.message = `Error searching for "${title}" by "${artist}"...`;
+            console.error("Error fetching lyrics:", error);
           });
-        })
-      .catch(error => {
-      this.message2 = 'Failed to fetch lyrics';
-      })  
+      } else {
+        alert("Please enter both song title and artist.");
+      }
+    },
+    // function to search for a song by title
+    generalSearch() {
+      // song name
+      const title = this.searchGeneral.trim();
+      
+      this.message3 = '';
+      this.message5 = '';
+
+      if (title) {
+        this.message2 = `Searching for "${title}"...`;
+        axios.get(`http://localhost:5001/genius/search/${encodeURIComponent(title)}`)
+          .then(response => {
+            this.scores = response.data;
+            this.message2 = '';
+          })
+          .catch(error => {
+            this.message2 = `Error searching for "${title}"...`;
+            console.error("Error fetching lyrics:", error);
+          });
+      } else {
+        alert("Please enter a song title.");
+      }
+    }, 
+    // function to play the song/start game; passes the song title and artist to the game
+    playSong(item,title, artist,recommendBool) {
+      this.showRecs = false;
+      this.message2 = '';
+      this.message5 = `Starting game: "${title}" by "${artist}"...`;
+      if (recommendBool === true) {
+        this.recommendation = [item];
+        this.scores = [];
+      } else {
+        this.recommendation = [];
+        this.scores = [item];
+      }
+
+      if (title && artist) {
+        axios.get(`http://localhost:5001/lyrics/${encodeURIComponent(title)}/${encodeURIComponent(artist)}`)
+          .then(response => {
+            this.message2 = '';
+            store.commit('setLyrics', response.data.lyrics);
+            store.commit('setTitle', title);
+            store.commit('setArtist', artist);
+            store.commit('setCover', response.data.cover);
+            this.$router.push({
+              path: '/game'
+            });
+          })
+          .catch(error => {
+            this.message5 = `Error starting game for "${title}" by "${artist}"...`;
+            console.error("Error fetching lyrics:", error);
+          });
+      } else {
+        alert("Something went wrong. Please try again.");
+      }
+    },
+    // returns a list of recommended songs based on the genre
+    recommended(genre, label) { 
+      this.message2 = '';
+      this.message5 = '';
+      this.message3 = `Looking for some songs in "${label}"...`;
+      axios.get(`http://localhost:5001/genius/genre/${encodeURIComponent(genre)}`)
+          .then(response => {
+            this.message3 = '';
+            this.recommendation = response.data;
+          })
+          .catch(error => {
+            this.message3 = `Error searching for "${genre}"...`;
+            console.error("Error fetching lyrics:", error);
+          });
+    },
+    created() {
+      this.submitSearch();
+      this.generalSearch();
+      this.playSong();
+      this.recommended();
     }
-  }
+  },
 };
 </script>
 
--- server/app.py
+++ server/app.py
@@ -4,17 +4,185 @@
 # flask run --port=5001 --debug
 #
 
-# from typing_extensions import Self
 from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
-from lyrics import *
+from genius import *
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from typing import List, Optional
+import os
+from werkzeug.security import generate_password_hash as hash_password, check_password_hash as verify_password
 
 # instantiate the app
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
+
+#
+# DATABASE
+#
+# this is a combination of tutorials and code from joslenne's activity
+# https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database <- flask DB setup
+# https://realpython.com/flask-connexion-rest-api-part-3/ <- linking tables
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mondegreen.db"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False # joslenne says we might need this? testing without
+
+# Initialize SQLAlchemy with Declarative Base
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+class Song(db.Model):
+    __tablename__ = "songs"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user: Mapped["User"] = relationship(back_populates="songs")
+    
+    # DATA:
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    artist: Mapped[str] = mapped_column(String, nullable=False)
+    img_path: Mapped[str] = mapped_column(String)
+    score: Mapped[int] = mapped_column(Integer, nullable=False)
+    
+    def __repr__(self):
+        return '<Song {} by {}>'.format(self.title, self.artist)
+    
+    def __init__(self, title: str, artist: str, img_path: str, score: int):
+        self.title = title
+        self.artist = artist
+        self.img_path = img_path
+        self.score = score
+        
+    def toJSON(self):
+        jason = {
+            'title': self.title,
+            'artist': self.artist,
+            'img_path': self.img_path,
+            'score': self.score
+        }
+        return jason
+    
+class User(db.Model):
+    __tablename__ = "users"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(64), index=True, unique=True)
+    password_hash: Mapped[Optional[str]] = mapped_column(String(256))
+    
+    # Understanding relationships:
+    # https://docs.sqlalchemy.org/en/20/orm/basic_relationships.html
+    songs: Mapped[List["Song"]] = relationship(Song, back_populates = "user")
+    
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+    
+    def __init__(self, username: str, password_hash: str = None):
+        self.username = username
+        if password_hash:
+            self.password_hash = password_hash    
+        
+    def toJSON(self):
+        songs = []
+        for song in self.songs:
+            songs.append(song.toJSON())
+        jason = {
+            'username': self.username,
+            'songs': songs
+        }
+        return jason
+
+def createSong(title: str, artist: str, score: int, user_id: int):
+    img_path = coverArt(title, artist)
+    new_song = Song(title, artist, img_path, score)
+    
+    active_user = db.session.get(User, user_id)
+    active_user.songs.append(new_song)
+    db.session.commit()
+    
+def editSong(id: int, score: int):
+    song = db.session.get(Song, id)
+    song.score = score
+    db.session.commit()
+
+# test song data from pre-database server testing
+SONGS = [
+    {
+        'title': 'symbol',
+        'artist': 'Adrianne Lenker',
+        'cover':  'https://images.genius.com/ea813421a18bde3bc39b73c110b0ab2c.300x300x1.jpg',
+        'score': 100
+    },
+    {
+        'title': 'Cloudbusting',
+        'artist': 'Kate Bush',
+        'cover':  'https://images.genius.com/c3e6f6097640bca27833078355fd647e.300x300x1.png',
+        'score': 75
+    },
+    {
+        'title': 'Simulation Swarm',
+        'artist': 'Big Thief',
+        'cover':  'https://images.genius.com/26084cc61b6b1849e2c762fd0ca709fc.300x300x1.png',
+        'score': 80
+    },
+    {
+        'title': 'Pump Up the Jam',
+        'artist': 'Technotronic',
+        'cover':  'https://images.genius.com/a3992138b1d56a1238150be0051cb321.300x300x1.png',
+        'score': 25
+    },
+    {
+        'title': 'Juna',
+        'artist': 'Clairo',
+        'cover':  'https://images.genius.com/6725f1000db2e875f4ce966f4144d41a.300x300x1.png',
+        'score': 37
+    },
+    {
+        'title': 'Hello Hello Hello',
+        'artist': 'Remi Wolf',
+        'cover':  'https://images.genius.com/9d9b505e394955ec1e750059846258c6.300x300x1.png',
+        'score': 100
+    },
+    {
+        'title': 'Linger',
+        'artist': 'The Cranberries',
+        'cover':  'https://images.genius.com/faeabffde6e1ce2c6ffafe4b5d01d4ab.300x300x1.png',
+        'score': 34
+    },
+    {
+        'title': 'Blister In The Sun',
+        'artist': 'Violent Femmes',
+        'cover':  'https://images.genius.com/af35b7cdb9d07071e3946098f542377b.300x295x1.jpg',
+        'score': 99
+    }
+]
+
+# Initialize database with sample data
+@app.before_request
+def setup():
+    with app.app_context():
+        db.create_all()
+        if not db.session.query(User).all():  # If database is empty, add a sample entry
+            u = User(username = 'redding')
+            for data in SONGS:
+                u.songs.append(
+                    Song(
+                        title = data.get('title'),
+                        artist = data.get('artist'),
+                        img_path = data.get('cover'),
+                        score = data.get('score')
+                    )
+                )
+            db.session.add(u)
+            db.session.commit()
 
 #
 # ROUTES
@@ -27,23 +195,149 @@ def index():
 def ping_pong():
     return jsonify('pong!')
 
-@app.route('/lyrics/list', methods = ['GET', 'POST'])
-def list():
-    songList = getList()
-    return jsonify({
-        'status': 'success',
-        'list': songList,
-    })
+@app.route('/admin')
+def admin():
+    all_users = db.session.query(User).all()
+    return render_template('admin.html', all_users = all_users)
 
     ### GENIUS API ROUTES
 # calls the getLyrics function from genius.py; returns the lyrics and cover of the song
 @app.route('/lyrics/<title>/<artist>', methods = ['GET', 'POST'])
 def lyrics(title = None, artist = None):
     songLyrics = getLyrics(title, artist)
+    songCover = getCover(title, artist)
     return jsonify({
         'status': 'success',
         'lyrics': songLyrics,
+        'cover': songCover
     })
+
+# calls the searchMulti function from genius.py; returns a list of songs that match the search term
+@app.route('/genius/search/<term>', methods = ['GET', 'POST'])
+def searchSong(term = None):
+    # parse data
+    results = searchMulti(term)
+    return results
+
+## eliminated because of the redundant searchSong function
+# @app.route('/genius/search2/<term>', methods = ['GET', 'POST'])
+# def searchSong2(term = None):
+#     results = searchMulti2(term)
+#     return results
+
+# calls the searchGenre function from genius.py; returns a list of songs in the genre
+@app.route('/genius/genre/<genre>/', methods = ['GET', 'POST'])
+def searchGenre2(genre = None):
+    results = searchGenre(genre)
+    return results
+
+    ### LOGIN SYSTEM
+@app.route('/login', methods=['POST'])
+def home():
+    data = request.json
+    username = data['username']
+    password = data['password']
+    
+    user_data = User.query.filter_by(username=username).first()
+    if user_data and verify_password(pwhash=user_data.password_hash, password=password):
+        # success, add to session
+        userid =  user_data.id
+        return jsonify({
+            'status': 'success',
+            'id': userid # TODO: BAD BAD SECURITY
+        })
+    # wrong password, failure
+    return jsonify({
+            'status': 'failure',
+            'message': 'wrong password'
+        })
+    
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+    username = data['username']
+    password = data['password']
+    # Check if the username already exists
+    if not User.query.filter_by(username=username).first():
+        # create new user
+        new_user = User(username, hash_password(password))
+        db.session.add(new_user)
+        db.session.commit()
+        # add to session
+        userid = new_user.id
+        return jsonify({
+            'status': 'success',
+            'id': userid # TODO: BAD BAD SECURITY
+        })
+    return jsonify({
+        'status': 'failure',
+        'message': 'account already exists'
+    })
+    
+@app.route('/logout', methods=['POST'])
+def logout():
+    # actually does nothing because of,, reasons
+    return jsonify({
+        'status': 'success'
+    })
+
+@app.route('/addsong', methods = ['GET', 'POST'])
+def addSong():
+    if request.method == 'POST':
+        data = request.json
+        userid = data['userid']
+        title = data['title']
+        artist = data['artist']
+        score = data['score']
+        u = db.session.get(User, userid)
+        
+        if(not u):
+            return jsonify({
+                'status': 'failure',
+                'message': 'user not found'
+            })
+        # query song table for: user id, song title, and artist
+        song = Song.query.filter_by(user_id = userid).filter_by(title = title).filter_by(artist = artist).first()
+ 
+        if song: # if the song exists
+            editSong(id = song.id, score = score)
+            return jsonify({
+                'status': 'success',
+                'message': 'update song'
+            })
+        createSong(title = title, artist = artist, score = score, user_id = userid)
+        return jsonify({
+                'status': 'success',
+                'message': 'new song'
+            })
+        
+@app.route('/songs', methods = ['POST'])
+def songs():
+    id = request.json['userid']
+    if not id:
+        return jsonify({
+            'status': 'failure',
+            'message': 'missing id'
+        })
+    u = db.session.get(User, id)
+    if not u:
+        return jsonify({
+            'status': 'failure',
+            'message': 'user not found'
+        })
+    user_songs = u.songs
+    songs = []
+    for song in user_songs:
+        songs.append(song.toJSON())
+    
+    return jsonify({
+        'status': 'success',
+        'songs': songs
+    })
+
+#
+# RUN
+#
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
